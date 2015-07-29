33e7b5aec3c97929f399ff52cf0ccb3076b2cf65
Daniel Osorio <danielcamiloosorio@gmail.com>
2015-07-28 21:38:18 -0500

    update EC filter

--- code/UniProt.R
+++ code/UniProt.R
@@ -1,5 +1,5 @@
 library(UniProt.ws)
-Proteome<-read.table("Documents/Proteomics/Results/Human_OligoDendocyte/OL_Proteins.txt",sep = "\t")
+Proteome<-read.table(commandArgs(TRUE)[1],sep = "\t")
 Proteome<-Proteome[which(Proteome$V22 == "Confident"),]
 Specie<-(UniProt.ws(taxId=9606))
 Specie@taxIdUniprots<- as.vector(Proteome$V2)
@@ -13,4 +13,5 @@ columns <- c("EC")
 kt <- c("UNIPROTKB")
 Data <- select(Specie, keys, columns, kt)
 EC<-Data[!is.na(Data$EC),]
+write.table(EC,"Documents/Proteomics/Results/Human_OligoDendocyte/EC_OL-OX.txt",sep = "\t")
 

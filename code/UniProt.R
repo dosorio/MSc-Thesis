8b7566add010c969426a340307a8fceaa45d4152
Daniel Osorio <danielcamiloosorio@gmail.com>
2015-07-29 15:53:59 -0500

    Update Proteomics

--- code/UniProt.R
+++ code/UniProt.R
@@ -4,34 +4,30 @@
 # Maestría en Bioinformática - Universidad Nacional de Colombia (Bogotá)
 # Laboratorio de Bioquímica Teórica y Bioinformática - Pontificia Universidad Javeriana (Bogotá)
 
-# Load the UniProt.ws package
+# Setting the working directory
+setwd("Documents/Proteomics/")
+
+# Loading the UniProt.ws package
 library(UniProt.ws)
 
-# Read the file
+# Reading the file
 Proteome<-read.table(commandArgs(TRUE)[1],sep = "\t")
-#Proteome<-read.table("Documents/Proteomics/results/Human_OligoDendocyte/OL-XO_Proteins.txt",sep = "\t")
+#Proteome<-read.table("results/Human_OligoDendocyte/OL-XO_Proteins.txt",sep = "\t")
 
-# Filter the confident proteins
+# Filtering the confident proteins
 Proteome<-Proteome[which(Proteome$V22 == "Confident"),]
 
-# Replace the protein list
+# Replacing the protein list
 Specie<-(UniProt.ws(taxId=9606))
 Specie@taxIdUniprots<- as.vector(Proteome$V2)
 
-
-ID<-keys(Specie,"UNIPROTKB")
-keys <- ID
-columns <- c("EC")
-kt <- c("UNIPROTKB")
+# Retrieving the UNIPROT EC numbers
 Data <- select(Specie, keys(Specie,"UNIPROTKB"), "EC", "UNIPROTKB")
-NoEC<-Data[is.na(Data$EC),]
+
+# Filtering data
+nEC<-Data[is.na(Data$EC),]
 EC<-Data[!is.na(Data$EC),]
-write.table(EC,"EC_OL-OX.txt",sep = "\t",col.names = FALSE,row.names = FALSE,quote = FALSE)
-write.table(NoEC,"NoEC_OL-OX.txt",sep = "\t",col.names = FALSE,row.names = FALSE,quote = FALSE)
 
-
-# RXN<-NULL
-# for (EC in paste("EC:",IDs,sep = "")){
-#   RXN<-append(RXN,(which(HMA$V5 == EC )))
-# }
-
+# Writing output files
+write.table(EC,"results/Human_OligoDendocyte/EC_OL-OX.txt",sep = "\t",col.names = FALSE,row.names = FALSE,quote = FALSE)
+write.table(nEC,"results/Human_OligoDendocyte/NoEC_OL-OX.txt",sep = "\t",col.names = FALSE,row.names = FALSE,quote = FALSE)

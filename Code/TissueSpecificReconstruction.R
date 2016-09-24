ffcee27b2c7a18cc49dc4d6499eeca6dd3964e6c
Daniel Osorio <dcosorioh@unal.edu.co>
2016-09-24 13:50:10 -0500

    Update masterThesis

--- Code/TissueSpecificReconstruction.R
+++ Code/TissueSpecificReconstruction.R
@@ -98,7 +98,8 @@ lowbnd(DMEM)[react_id(DMEM) == 'EX_estradiol(e)'] <- -1
 lowbnd(DMEM)[react_id(DMEM) == 'EX_nh4(e)'] <- -100
 
 
-Reactions_Flux <- (RECON[getFluxDist(optimizeProb(DMEM))!=0,3])
+Reactions_Flux <-(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])
+DMEM@obj_coef <- rep(0,DMEM@react_num)
 
 DMEM <- addReact(DMEM, id="MC", met=c("glu_L[e]","ca2[e]","gln_L[e]"),
                  Scoef=c(-1,-1,1), reversible=FALSE,

cf6c4fc849e993734073a55fd04b7c77bc72ac05
Daniel Osorio <dcosorioh@unal.edu.co>
2016-10-28 17:14:57 -0500

    Update masterThesis

--- Code/GliotransmitterRelease.R
+++ Code/GliotransmitterRelease.R
@@ -81,8 +81,8 @@ sink()
 matureAstrocyte_Model <- readSBMLmod("Results/matureAstrocyte.xml")
 
 # Adding Palmitate
-lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_hdca(e)'] <- -0.226
-uppbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_hdca(e)'] <- -0.226
+lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_hdca(e)'] <- -0.208
+uppbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_hdca(e)'] <- -0.208
 
 
 # Minimizing the total absolute fluxes MTF (Evaluando las múltiples posibles soluciones)
@@ -164,59 +164,18 @@ convert2sbml(matureAstrocyte_Model,"Results/matureAstrocyte_Tibolone.xml")
 matureAstrocyte_Model <- readSBMLmod("Results/matureAstrocyte_Tibolone.xml")
 
 # Adding Palmitate
-lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_hdca(e)'] <- -0.226
-uppbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_hdca(e)'] <- -0.226
+lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_hdca(e)'] <- -0.208
+uppbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_hdca(e)'] <- -0.208
 lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_estradiol(e)'] <- -1
 lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_prgstrn(e)'] <- -1
 lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_tststerone(e)'] <- -1
 lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_crtsl(e)'] <- -1
 lowbnd(matureAstrocyte_Model)[react_id(matureAstrocyte_Model) == 'EX_aldstrn(e)'] <- -1
 
-# Adding Tibolone
-matureAstrocyte_Model <- addExchReact(matureAstrocyte_Model,met = "tib[e]",lb = -1,ub = -1)
-matureAstrocyte_Model <- addReact(model = matureAstrocyte_Model,
-                                  id = "TiboloneMetabolism",
-                                  met = c("tib[e]","OHtib_3a[e]","OHtib_3b[e]","d4_tib[e]"),
-                                  Scoef = c(-1,1,1,1),
-                                  reversible = FALSE,
-                                  ub = 1000)
-matureAstrocyte_Model <- addReact(model = matureAstrocyte_Model,
-                                  id = "EstrogenAgonist1",
-                                  met = c("OHtib_3a[e]","estradiol[c]"),
-                                  Scoef = c(-1,1),
-                                  reversible = FALSE,
-                                  ub = 1000)
-matureAstrocyte_Model <- addReact(model = matureAstrocyte_Model,
-                                  id = "EstrogenAgonist2",
-                                  met = c("OHtib_3b[e]","estradiol[c]"),
-                                  Scoef = c(-1,1),
-                                  reversible = FALSE,
-                                  ub = 1000)
-matureAstrocyte_Model <- addReact(model = matureAstrocyte_Model,
-                                  id = "ProgesteroneAndAndrogenicAgonist",
-                                  met = c("d4_tib[e]","prgstrn[e]","tststerone[e]","prgstrn[c]","tststerone[c]"),
-                                  Scoef = c(-1,-2,-1,2,1),
-                                  reversible = FALSE,
-                                  ub = 1000)
-matureAstrocyte_Model <- addReact(model = matureAstrocyte_Model,
-                                  id = "ProgesteroneAndAndrogenicAntagonist",
-                                  met = c("OHtib_3a[e]","OHtib_3b[e]","prgstrn[c]","tststerone[c]","prgstrn[e]","tststerone[e]"),
-                                  Scoef = c(-1,-1,-1,-1,1,1),
-                                  reversible = FALSE,
-                                  ub = 1000)
-matureAstrocyte_Model <- addReact(model = matureAstrocyte_Model,
-                                  id = "MineralocorticoidAntagonist",
-                                  met = c("OHtib_3a[e]","OHtib_3b[e]","crtsl[c]","aldstrn[c]","crtsl[e]","aldstrn[e]"),
-                                  Scoef = c(-1,-1,-1,-1,1,1),
-                                  reversible = FALSE,
-                                  ub = 1000)
-
-plot(robAna(matureAstrocyte_Model,"Ex_tib[e]",rng = c(-1,0)))
-# Minimizing the total absolute fluxes MTF (Evaluando las múltiples posibles soluciones)
-dir.create("Results/inflammatedAstrocyte")
+dir.create("Results/Tibolone")
 
 # BO Inflammated
-sink("Results/inflammatedAstrocyte/BO.txt")
+sink("Results/Tibolone/BO.txt")
 model_FBA <- optimizeProb(matureAstrocyte_Model)
 model_FBA
 model_MTF <- optimizeProb(matureAstrocyte_Model, algorithm = "mtf", wtobj = mod_obj(model_FBA))
@@ -224,7 +183,7 @@ print(getNetFlux(getFluxDist(model_MTF,findExchReact(matureAstrocyte_Model))))
 sink()
 
 # Glu-Gln
-sink("Results/inflammatedAstrocyte/Glu-Gln.txt")
+sink("Results/Tibolone/Glu-Gln.txt")
 matureAstrocyte_Model <- addReact(matureAstrocyte_Model, id="MC", met=c("glu_L[e]","gln_L[e]"),
                                   Scoef=c(-1,1), reversible=FALSE,
                                   lb=0, ub=1000, obj=1)
@@ -235,7 +194,7 @@ print(getNetFlux(getFluxDist(model_MTF,findExchReact(matureAstrocyte_Model))))
 sink()
 
 # Gly-serD
-sink("Results/inflammatedAstrocyte/Gly-SerD.txt")
+sink("Results/Tibolone/Gly-SerD.txt")
 matureAstrocyte_Model <- addReact(matureAstrocyte_Model, id="MC", met=c("gly[e]","ser_D[e]"),
                                   Scoef=c(-1,1), reversible=FALSE,
                                   lb=0, ub=1000, obj=1)
@@ -246,7 +205,7 @@ print(getNetFlux(getFluxDist(model_MTF,findExchReact(matureAstrocyte_Model))))
 sink()
 
 # Glc-Lactate
-sink("Results/inflammatedAstrocyte/Glc-Lactate.txt")
+sink("Results/Tibolone/Glc-Lactate.txt")
 matureAstrocyte_Model <- addReact(matureAstrocyte_Model, id="MC", met=c("glc_D[e]","lac_L[e]"),
                                   Scoef=c(-1,2), reversible=FALSE,
                                   lb=0, ub=1000, obj=1)
@@ -258,7 +217,7 @@ sink()
 
 
 # Glc-ATP
-sink("Results/inflammatedAstrocyte/Glc-ATP.txt")
+sink("Results/Tibolone/Glc-ATP.txt")
 matureAstrocyte_Model <- addReact(matureAstrocyte_Model, id="MC", met=c("glc_D[e]","atp[e]"),
                                   Scoef=c(-1,36), reversible=FALSE,
                                   lb=0, ub=1000, obj=1)
@@ -269,7 +228,7 @@ print(getNetFlux(getFluxDist(model_MTF,findExchReact(matureAstrocyte_Model))))
 sink()
 
 # Cys-GTHRD
-sink("Results/inflammatedAstrocyte/Cys-GTHRD.txt")
+sink("Results/Tibolone/Cys-GTHRD.txt")
 matureAstrocyte_Model <- addReact(matureAstrocyte_Model, id="MC", met=c("cys_L[e]","glu_L[c]","gly[c]","gthrd[e]"),
                                   Scoef=c(-1,-1,-1,1), reversible=FALSE,
                                   lb=0, ub=1000, obj=1)
@@ -279,6 +238,7 @@ model_MTF <- optimizeProb(matureAstrocyte_Model, algorithm = "mtf", wtobj = mod_
 print(getNetFlux(getFluxDist(model_MTF,findExchReact(matureAstrocyte_Model))))
 sink()
 
+#### Tibolone
 
 # Extraigo rutas metabolicas involucradas en la diferenciación de astrocito maduro
 # AstrocyteMetabolism<-enrichPathway(unique(na.omit(Astrocyte_Genes$ENTREZ_GENE[Astrocyte_Genes$EC!=""])),organism = "human",readable = TRUE,pvalueCutoff = 0.05)
--- Code/TissueSpecificReconstruction.R
+++ Code/TissueSpecificReconstruction.R
@@ -300,66 +300,64 @@ Astrocyte_Reconstruction <- mapReactions(reactionList = unique(c(Enrichment,Astr
                                          by = "REACTION")
 # 
 # # Tibolone Mecanism
-# lowbnd(DMEM)[react_id(DMEM) == 'EX_estradiol(e)'] <- -1
-# lowbnd(DMEM)[react_id(DMEM) == 'EX_crtsl(e)'] <- -1
-# lowbnd(DMEM)[react_id(DMEM) == 'EX_aldstrn(e)'] <- -1
-# lowbnd(DMEM)[react_id(DMEM) == 'EX_prgstrn(e)'] <- -1
-# lowbnd(DMEM)[react_id(DMEM) == 'EX_tststerone(e)'] <- -1
-# RECON$LOWER.BOUND <- DMEM@lowbnd
-# RECON$UPPER.BOUND <- DMEM@uppbnd
-# DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","hdca[e]","h2o2[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- RECON[getFluxDist(optimizeProb(DMEM))!=0,3]
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","hdca[e]","o2s[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("crtsl[e]","hdca[e]","h2o2[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("crtsl[e]","hdca[e]","o2s[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("aldstrn[e]","hdca[e]","o2s[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("aldstrn[e]","hdca[e]","h2o2[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("prgstrn[e]","hdca[e]","o2s[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("prgstrn[e]","hdca[e]","h2o2[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# 
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("tststerone[e]","hdca[e]","o2s[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# 
-# DMEM <- addReact(DMEM, id="MC", met=c("tststerone[e]","hdca[e]","h2o2[e]"),
-#                  Scoef=c(-1,-1,1), reversible=FALSE,
-#                  lb=0, ub=1000, obj=1)
-# Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
-# Tibolone <- mapReactions(reactionList = Tibolone[!Tibolone%in%Astrocyte_Reconstruction$REACTION],referenceData = RECON,by = "REACTION")
-# write.csv2(x = Tibolone,file = "Results/TiboloneReactions.csv",row.names = FALSE)
+Tibolone <- NULL
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","h2o2[m]","h2o[m]"),
+                 Scoef=c(-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","o2s[m]","h2o[m]"),
+                 Scoef=c(-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","h2o2[x]","h2o[x]"),
+                 Scoef=c(-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","o2s[x]","h2o[x]"),
+                 Scoef=c(-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","h2o2[c]","h2o[c]"),
+                 Scoef=c(-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","o2s[c]","h2o[c]"),
+                 Scoef=c(-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","glu_L[e]","gln_L[e]"),
+                 Scoef=c(-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","gly[e]","ser_D[e]"),
+                 Scoef=c(-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","glc_D[e]","lac_L[e]"),
+                 Scoef=c(-1,-1,2), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","glc_D[e]","atp[e]"),
+                 Scoef=c(-1,-1,36), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+DMEM <- addReact(DMEM, id="MC", met=c("estradiol[e]","cys_L[e]","glu_L[c]","gly[c]","gthrd[e]"),
+                 Scoef=c(-1,-1,-1,-1,1), reversible=FALSE,
+                 lb=0, ub=1000, obj=1)
+Tibolone <- unique(c(Tibolone,(RECON[getFluxDist(optimizeProb(DMEM))!=0,3])))
+
+Tibolone <- mapReactions(reactionList = Tibolone[!Tibolone%in%Astrocyte_Reconstruction$REACTION],referenceData = RECON,by = "REACTION")
+write.csv2(x = Tibolone,file = "Results/TiboloneReactions.csv",row.names = FALSE)
 # #
-# Astrocyte_Reconstruction <- rbind(Astrocyte_Reconstruction,Tibolone)
+#Astrocyte_Reconstruction <- rbind(Astrocyte_Reconstruction,Tibolone)
 write.csv2(x = Astrocyte_Reconstruction,file = "Results/Astrocyte.csv",row.names = FALSE)
 convert2sbml(Astrocyte_Reconstruction,"Results/Astrocyte.xml")
